62d63d619fbb902e1da8035e472367fc5eb5f2f1
Marinus Sewalt <marinus.sewalt@gmail.com>
2023-06-08 01:12:25 +0800

    Smoke Test

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+    let (reader, mut writer) = socket.split();
+
+    let mut reader = BufReader::new(reader);
+
+    let mut buf = Vec::new();
+
+    reader.read_to_end(&mut buf).await.unwrap();
+    writer.write_all(buf.as_slice()).await.unwrap();
+}
