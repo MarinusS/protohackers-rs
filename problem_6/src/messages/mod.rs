c32a35f21f8da3095e91dc283b5cbf0b2b139949
marinus <marinus@vps-3ec6add5.vps.ovh.ca>
2023-06-11 17:38:05 +0000

    Implemented Plate msg

--- problem_6/src/messages/mod.rs
+++ problem_6/src/messages/mod.rs
@@ -1,11 +1,14 @@
 mod error;
-mod factory;
 mod i_am_camera;
+mod plate;
+
+mod factory;
 
 pub use factory::*;
 
 #[derive(PartialEq, Debug)]
 pub enum Message {
     Error { msg: String },
+    Plate { plate: String, timestamp: u32 },
     IAmCamera { road: u16, mile: u16, limit: u16 },
 }
---
+++ problem_6/src/messages/plate.rs
@@ -0,0 +1,84 @@
+use super::Message;
+use super::MessageFactory;
+
+pub const ID_BYTE: u8 = 0x20;
+
+pub struct PlateFactory {
+    buffer: Vec<u8>,
+    plate_len: usize,
+    cursor: usize,
+}
+
+impl MessageFactory for PlateFactory {
+    fn new() -> Self {
+        PlateFactory {
+            buffer: Vec::new(),
+            cursor: 0,
+            plate_len: 0,
+        }
+    }
+
+    fn push(&mut self, data: u8) -> Option<Message> {
+        if self.buffer.is_empty() {
+            self.buffer.extend(vec![0; data as usize + 4]);
+            self.plate_len = data as usize;
+        } else {
+            self.buffer[self.cursor] = data;
+            self.cursor += 1;
+        }
+
+        if self.cursor == self.buffer.len() {
+            Some(Message::Plate {
+                plate: String::from_utf8_lossy(&self.buffer[..self.plate_len]).to_string(),
+                timestamp: u32::from_be_bytes(self.buffer[self.plate_len..].try_into().unwrap()),
+            })
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::Message::Plate;
+    use super::*;
+
+    #[test]
+    fn test_push() {
+        struct Test {
+            test_data: Vec<u8>,
+            expected: Message,
+        }
+
+        let tests = vec![
+            Test {
+                test_data: vec![0x04, 0x55, 0x4e, 0x31, 0x58, 0x00, 0x00, 0x03, 0xe8],
+                expected: Plate {
+                    plate: "UN1X".to_string(),
+                    timestamp: 1000,
+                },
+            },
+            Test {
+                test_data: vec![
+                    0x07, 0x52, 0x45, 0x30, 0x35, 0x42, 0x4b, 0x47, 0x00, 0x01, 0xe2, 0x40,
+                ],
+                expected: Plate {
+                    plate: "RE05BKG".to_string(),
+                    timestamp: 123456,
+                },
+            },
+        ];
+
+        for test in tests {
+            let mut fact = PlateFactory::new();
+            for &byte in test.test_data[..test.test_data.len() - 1].iter() {
+                assert!(fact.push(byte).is_none());
+            }
+
+            let msg = fact.push(*test.test_data.last().unwrap());
+            assert!(msg.is_some());
+            assert_eq!(msg.unwrap(), test.expected);
+        }
+    }
+}
